6ac265f1985dbd42182cc3c7b68fe9ff3f2b26c9
saicharan <saicharan637@gmail.com>
2022-01-01 19:16:07 -0500

    Updates

---
+++ precache-manifest.0598ad802424f302a2c416c4e375bef2.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d415bbb41e5d9e27aed7289f0a590b5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9c88d85da4724b53c38b",
+    "url": "/static/css/main.a90790e1.chunk.css"
+  },
+  {
+    "revision": "21a7eaf4091cd4bab00f",
+    "url": "/static/js/2.6ef27e2a.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/static/js/2.6ef27e2a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9c88d85da4724b53c38b",
+    "url": "/static/js/main.093649a0.chunk.js"
+  },
+  {
+    "revision": "f52ba179432c3be28857",
+    "url": "/static/js/runtime-main.554504a0.js"
+  },
+  {
+    "revision": "f8fac3e63ca3b44bf5173ac299edf452",
+    "url": "/static/media/resume.f8fac3e6.pdf"
+  },
+  {
+    "revision": "d94199a2235b6b4ac40db9c25f7fe81c",
+    "url": "/static/media/saicharan.d94199a2.png"
+  }
+]);
